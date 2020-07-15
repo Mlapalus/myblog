6679a4bc2fcf5ca7d78478cd41ac78bb8aaa91bc
mlapalus <mlapalus@yahoo.fr>
2020-07-15 05:30:05 +0200

    Commit: pythonanywhere in allowedHost

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1ji&+ps6kb0wq2&oip5yg@dcmah!-dl)egdo$51u*(ryr=kt(2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
